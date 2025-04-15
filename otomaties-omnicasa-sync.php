8e997556272f2e46e9b8c29e001c307481cef68e
Tom Broucke <tom@tombroucke.be>
2025-04-15 14:35:53 +0200

    🔼 chore(bump): Bump version to 1.1.1

--- otomaties-omnicasa-sync.php
+++ otomaties-omnicasa-sync.php
@@ -7,7 +7,7 @@ use Otomaties\Omnicasa\Plugin;
 /**
  * Plugin Name:       Otomaties Omnicasa Sync
  * Description:       Sync Omnicasa properties with WordPress
- * Version:           1.1.0
+ * Version:           1.1.1
  * Author:            Tom Broucke
  * Author URI:        https://tombroucke.be/
  * License:           GPL-2.0+
